e07cfd50e07579af260be18ff02d4249f618df32
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-04-18 07:10:57 +0200

    updating kanban tests

--- src/components/KanbanBoard/__test__/KanbanBoard.test.js
+++ src/components/KanbanBoard/__test__/KanbanBoard.test.js
@@ -1,17 +1,134 @@
-import { render, screen } from "@testing-library/react";
-import KanbanBoard from "../KanbanBoard";
-import { Button } from "react-bootstrap";
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { store } from "../../../redux/store/index.js";
+import configureStore from "redux-mock-store";
+import KanbanBoard from "../KanbanBoard.jsx";
 
-test("renders Load issues button", async () => {
-  render(
-    <Provider store={store}>
-      <KanbanBoard />
-    </Provider>
-  );
-  const btnElement = await screen.findByRole("button", {
-    name: /Load issues/i,
+describe("KanbanBoard component", () => {
+  const mockStore = configureStore([]);
+
+  it("renders without crashing", () => {
+    const store = mockStore({
+      reducer: {
+        data: [
+          {
+            id: "todo",
+            title: "To Do",
+            tasks: [
+              {
+                id: "task-1",
+                title: "Task 1",
+                updates: [{ editor: "User 1", date: "2023-04-16" }],
+              },
+            ],
+          },
+          {
+            id: "in-progress",
+            title: "In Progress",
+            tasks: [],
+          },
+          {
+            id: "done",
+            title: "Done",
+            tasks: [],
+          },
+        ],
+      },
+    });
+    render(
+      <Provider store={store}>
+        <KanbanBoard />
+      </Provider>
+    );
   });
-  expect(btnElement).toBeInTheDocument();
+
+  it("updates URL input value on change", () => {
+    const store = mockStore({
+      reducer: {
+        data: [
+          {
+            id: "todo",
+            title: "To Do",
+            tasks: [
+              {
+                id: "task-1",
+                title: "Task 1",
+                updates: [{ editor: "User 1", date: "2023-04-16" }],
+              },
+            ],
+          },
+          {
+            id: "in-progress",
+            title: "In Progress",
+            tasks: [],
+          },
+          {
+            id: "done",
+            title: "Done",
+            tasks: [],
+          },
+        ],
+      },
+    });
+    render(
+      <Provider store={store}>
+        <KanbanBoard />
+      </Provider>
+    );
+
+    const urlInput = screen.getByPlaceholderText("Enter REPO url");
+    fireEvent.change(urlInput, {
+      target: { value: "https://github.com/user/repo/issues" },
+    });
+
+    expect(urlInput.value).toBe("https://github.com/user/repo/issues");
+  });
+
+  it("dispatches 'fetchData' action on 'Load issues' button click", () => {
+    const store = mockStore({
+      reducer: {
+        data: [
+          {
+            id: "todo",
+            title: "To Do",
+            tasks: [
+              {
+                id: "task-1",
+                title: "Task 1",
+                updates: [{ editor: "User 1", date: "2023-04-16" }],
+              },
+            ],
+          },
+          {
+            id: "in-progress",
+            title: "In Progress",
+            tasks: [],
+          },
+          {
+            id: "done",
+            title: "Done",
+            tasks: [],
+          },
+        ],
+      },
+    });
+    render(
+      <Provider store={store}>
+        <KanbanBoard />
+      </Provider>
+    );
+
+    const urlInput = screen.getByPlaceholderText("Enter REPO url");
+    fireEvent.change(urlInput, {
+      target: { value: "https://github.com/user/repo/issues" },
+    });
+
+    const loadIssuesButton = screen.getByText("Load issues");
+    fireEvent.click(loadIssuesButton);
+
+    expect(store.getActions()).toContainEqual({
+      type: "FETCH_DATA",
+      payload: "https://github.com/user/repo/issues",
+    });
+  });
 });
